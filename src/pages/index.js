13fa8a996bf672825ddec1dd86faa79f3769b927
Kevin V <kevin.vannimmen@gmail.com>
2020-01-14 10:41:08 +0100

    second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,16 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+const Home = () => {
+  return (
+    <div style={{ color: `purple`, fontSize: `72px` }}>
+      <Link to="/contact/">Contact</Link>
+      <h1> Hello Gatsby!</h1>
+      <p>What a world.</p>{" "}
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+      <button class="primary-button">Click me</button>
+    </div>
+  )
+}
+
+export default Home
